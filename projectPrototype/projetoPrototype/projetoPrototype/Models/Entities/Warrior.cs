ddd45878f52a6e0bd7b310fd63cbef50370d5334
Alexandre Matos <matosalexandre795@gmail.com>
2026-04-13 22:17:40 -0300

    Prototype Character e Concrete Warrior

---
+++ projectPrototype/projetoPrototype/projetoPrototype/Models/Entities/Warrior.cs
@@ -0,0 +1,21 @@
+﻿using projetoPrototype.Models.Prototypes;
+
+namespace projetoPrototype.Models.Entities
+{
+    // Concrete Prototype
+    public class Warrior : Character
+    {
+        public Warrior(string name, int level, string weapon, int strength) : base(name, level, "Warrior") 
+        {
+            Weapon = weapon;
+            Strength = strength;
+        }
+        public string Weapon { get; set; }
+        public int Strength { get; set; }
+        public override Character Clone()
+        {
+            return (Character)this.MemberwiseClone();
+            //return new Warrior(Name, Level, Weapon, Strength);
+        }
+    }
+}
---
+++ projectPrototype/projetoPrototype/projetoPrototype/Models/Prototypes/Character.cs
@@ -0,0 +1,17 @@
+﻿namespace projetoPrototype.Models.Prototypes
+{
+
+    // Prototype
+    public abstract class Character
+    {
+        protected Character(string name, int level, string characterClass) { 
+            Name = name;
+            Level = level;
+            CharacterClass = characterClass;
+        }
+            public string Name { get; set; }
+            public int Level { get; set; }
+            public string CharacterClass { get; set; }
+        public abstract Character Clone();
+    }
+}
