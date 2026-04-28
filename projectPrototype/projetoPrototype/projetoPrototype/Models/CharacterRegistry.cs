ba65222abdad116d835cadca8816ee09933d3c06
Alexandre Matos <matosalexandre795@gmail.com>
2026-04-27 21:38:29 -0300

    Adding Test and CharacterRegistry

---
+++ projectPrototype/projetoPrototype/projetoPrototype/Models/CharacterRegistry.cs
@@ -0,0 +1,21 @@
+﻿using projetoPrototype.Models.Prototypes;
+
+namespace projetoPrototype.Models
+{
+    public class CharacterRegistry
+    {
+            private Dictionary<string, Character> prototypes = new();
+
+            public void AddPrototype(
+                 string key,
+                 Character character)
+            {
+                prototypes[key] = character;
+            }
+
+            public Character CloneCharacter(string key)
+            {
+                return prototypes[key].Clone();
+            }
+        }
+}
